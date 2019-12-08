e41c52f1482ff696086d8c273ac0f98c2a775160
Alexa <alexa.fazio04@gmail.com>
2019-12-07 19:33:53 -0500

    finished exercise 3

--- js/shapes.js
+++ js/shapes.js
@@ -19,6 +19,8 @@ window.onload = function() {
     // equivalent set of six event listeners for your solutions. the first one is done for you.
 
     document.getElementById("hello").onclick = sayHello;
+    document.getElementById("rectangle").onclick = drawRectangle;
+    document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
 }
 
 /*
@@ -28,12 +30,16 @@ window.onload = function() {
 const sayHello = function() {
     let ctx = document.getElementById('student-canvas-1')
     let context = ctx.getContext('2d');
-  let text = prompt("Message:");
+    let text = prompt("Message:");
 
-  context.clearRect(0, 0, ctx.width, ctx.height); //change to real width and height
+  context.clearRect(0, 0, ctx.width, ctx.height);
   if (text.length <= 50 && text.length >= 1) {
     context.font = "48px sans-serif";
     context.strokeText(text, 30, 70, 994);
+  }
+  else {
+    alert("Your message is too long. Keep it under 50 characters.");
+    let text = prompt("Message: ");
   }
 };
 
@@ -43,6 +49,57 @@ const sayHello = function() {
 
 const drawRectangle = function() {
     // write your exercise 2 code here
+
+    let ctx = document.getElementById('student-canvas-2')
+    let context = ctx.getContext('2d');
+    context.clearRect(0, 0, ctx.width, ctx.height);
+    let width = prompt("Width:");
+    let height = prompt("Height:");
+    let x = prompt("X:");
+    let y = prompt("Y:");
+
+    width = Number(width);
+    height = Number(height);
+    x = Number(x);
+    y = Number(y);
+
+    const canvas_width = 1024;
+    const canvas_height = 512;
+
+    function promptDimensions() {
+        width = prompt("Width:");
+        height = prompt("Height:");
+        x = prompt("X:");
+        y = prompt("Y:");
+        width = Number(width);
+        height = Number(height);
+        x = Number(x);
+        y = Number(y);
+    }
+    //need to fix how to stop the code
+    while (width >= canvas_width || width < 1) {
+        alert("Your width must be between 1 and 1024.");
+        promptDimensions();
+    }
+    while (height >= canvas_height || height < 1) {
+        alert("Your height must be between 1 and 512.");
+        promptDimensions();
+    }
+    while (x >= canvas_width || x < 1) {
+        alert("Your x-coordinate must be between 1 and 1024.");
+        promptDimensions();
+    }
+    while (y >= canvas_height || y < 1) {
+        alert("Your x-coordinate must be between 1 and 512.");
+        promptDimensions();
+    }
+    while (x + width > canvas_width || y + height > canvas_height) {
+        alert("Your rectangle won't fit on the canvas.");
+        promptDimensions();
+    }
+    
+    context.strokeRect(x, y, width, height);
+
 };
 
 /*
@@ -51,6 +108,21 @@ const drawRectangle = function() {
 
 const drawColoredRectangle = function() {
     // write your exercise 3 code here
+    let ctx = document.getElementById('student-canvas-3')
+    let context = ctx.getContext('2d');
+    context.clearRect(0, 0, ctx.width, ctx.height);
+    let color = prompt("Pick a color.");
+    color = String(color);
+    color = color.toLowerCase();
+    while (color != "black" && color != "blue" && color != "green" && color != "orange" && color != "purple" && color != "red" && color != "yellow") {
+        alert("This is an unsupported color.");
+        color = prompt("Pick a color.");
+        color = String(color);
+        color = color.toLowerCase();
+    }
+    context.fillStyle = color;
+    context.fillRect(10, 10, 100, 50);
+    
 };
 
 /*
